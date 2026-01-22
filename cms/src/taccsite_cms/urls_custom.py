158fa2c5d9199e13307ffd7ca162c2777ccddc8f
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2026-01-22 15:05:39 -0600

    feat: allow CMS user to reset or change password

--- cms/src/taccsite_cms/urls_custom.py
+++ cms/src/taccsite_cms/urls_custom.py
@@ -1,5 +1,12 @@
-from django.urls import path, include
+from django.urls import path, re_path, include
 
 custom_urls = [
+
     path('example_app/', include('apps.example_app.urls', namespace='example_app')),
+
+    # DJANGO
+    # To allow CMS user to change or reset password
+    # TODO: Make CMS-only Portal Use TAPIs Auth & Do Not Do This
+    path('accounts/', include('django.contrib.auth.urls')),
+
 ]
