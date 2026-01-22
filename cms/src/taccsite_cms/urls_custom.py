b1d298e3e8fef130d18cd8b32a5877c5496b1cbe
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2026-01-22 16:00:17 -0600

    Revert "feat: allow CMS user to reset or change password"
    This reverts commit 158fa2c5d9199e13307ffd7ca162c2777ccddc8f.

--- cms/src/taccsite_cms/urls_custom.py
+++ cms/src/taccsite_cms/urls_custom.py
@@ -1,13 +1,8 @@
-from django.urls import path, re_path, include
+from django.urls import path, include
 
 custom_urls = [
 
     path('example_app/', include('apps.example_app.urls', namespace='example_app')),
-
-    # DJANGO
-    # To allow CMS user to change or reset password
-    # TODO: Make CMS-only Portal Use TAPIs Auth & Do Not Do This
-    path('accounts/', include('django.contrib.auth.urls')),
 
     # To support `taggit_autosuggest` (from `djangocms-blog`)
     re_path(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
